bec5d06f57feba0804dded369fb74d27cdd828fa
Drovyng <121764701+Drovyng@users.noreply.github.com>
2024-04-19 11:56:54 +0300

    Just Let Me Fix This

--- imaginazer.py
+++ imaginazer.py
@@ -48,33 +48,30 @@ def toImage(week:WeekData, getPairs:list[str], getTeachers:list[str]) -> BytesIO
     for i in range(6):
         day = week[i]
         offY = sizey / 4 + sizeY * i
+        imgDraw.rectangle((offX, offY, sizeX + offX, s48 + offY), None, (100, 100, 100), width)
 
         imgDraw.rectangle((offX, s48 + offY, sizeX + offX, s48 + sizey * 3 + offY), None, (100, 100, 100), width)
+        imgDraw.line((offX, s48 + sizey + offY, sizeX + offX, s48 + sizey + offY), (100, 100, 100), width)
+        imgDraw.line((offX, s48 + sizey * 2 + offY, sizeX + offX, s48 + sizey * 2 + offY), (100, 100, 100), width)
+        imgDraw.line((s300 + offX, s48 + offY, s300 + offX, s48 + sizey * 3 + offY), (100, 100, 100), width)
+        imgDraw.line((s500 + offX, s48 + offY, s500 + offX, s48 + sizey * 3 + offY), (100, 100, 100), width)
 
         topText = days[i]
-        if day[0] == -1:
+        if day[0] != -1:
             day[0] = 0
         topText += f" | Приходить к {day[0]+1}-й паре"
-        
         j = -1
         for pair in [day[1], day[2], day[3]]:
             j += 1
             if min(pair) == -1:
                 continue
-            
             imgDraw.text((s6 + offX, s65 + sizey * j + offY), pairs[pair[0]], (0, 0, 0), font=font)
             imgDraw.text((s306 + offX, s65 + sizey * j + offY), teachers[pair[1]], (0, 0, 0), font=font)
             imgDraw.text((s532 + offX - font.getlength(cabinets[pair[2]])/2, s65 + sizey * j + offY), cabinets[pair[2]], (0, 0, 0), font=font)
-            
             if j == 0:
-                imgDraw.rectangle((s300 + offX, s65 + sizey * j + offY, sizeX + offX * 2, s65 + sizey * (j+1) + offY), None, (100, 100, 100), width)
-
-        imgDraw.text((int(sizeX/2 - font.getlength(topText) / 2) + offX, s10 + offY), topText, (0, 0, 0), font=font)
+                imgDraw.rectangle((s306 + offX, s65 + sizey * j + offY, sizeX + offX * 2, s65 + sizey * (j+1) + offY), None, (200, 200, 200))
         
-        imgDraw.line((offX, s48 + sizey + offY, sizeX + offX, s48 + sizey + offY), (100, 100, 100), width)
-        imgDraw.line((offX, s48 + sizey * 2 + offY, sizeX + offX, s48 + sizey * 2 + offY), (100, 100, 100), width)
-        imgDraw.line((s300 + offX, s48 + offY, s300 + offX, s48 + sizey * 3 + offY), (100, 100, 100), width)
-        imgDraw.line((s500 + offX, s48 + offY, s500 + offX, s48 + sizey * 3 + offY), (100, 100, 100), width)
+        imgDraw.text((int(sizeX/2 - font.getlength(topText) / 2) + offX, s10 + offY), topText, (0, 0, 0), font=font)
 
     output = BytesIO()
     img.save(output, format='PNG')
@@ -110,11 +107,6 @@ def toImageDay(day:DayData, dayText:str, getPairs:list[str], getTeachers:list[st
     img = Image.new("RGB", (sizeX + offX * 2, sizeY), (200, 200, 200))
     imgDraw = ImageDraw.Draw(img, "RGB")
     font = ImageFont.truetype("times.ttf", int(20 * scale))
-    
-    
-    imgDraw.rectangle((offX, offY, sizeX + offX, s48 + offY), (100, 100, 100), width)
-    imgDraw.rectangle(None, (offX, s48 + offY, sizeX + offX, s48 + sizey * 3 + offY), (100, 100, 100), width)
-
 
     offY = sizey / 4
 
@@ -129,19 +121,20 @@ def toImageDay(day:DayData, dayText:str, getPairs:list[str], getTeachers:list[st
         imgDraw.text((s6 + offX, s65 + sizey * j + offY), pairs[pair[0]], (0, 0, 0), font=font)
         imgDraw.text((s306 + offX, s65 + sizey * j + offY), teachers[pair[1]], (0, 0, 0), font=font)
         imgDraw.text((s532 + offX - font.getlength(cabinets[pair[2]])/2, s65 + sizey * j + offY), cabinets[pair[2]], (0, 0, 0), font=font)
-
-        
         if j == 0:
-            imgDraw.rectangle((s300 + offX, s65 + sizey * j + offY, sizeX + offX * 2, s65 + sizey * (j+1) + offY), None, (100, 100, 100), width)
-
+            imgDraw.rectangle((s306 + offX, s65 + sizey * j + offY, sizeX + offX * 2, s65 + sizey * (j+1) + offY), None, (200, 200, 200))
+    
     imgDraw.text((int(sizeX/2 - font.getlength(dayText) / 2) + offX, s10 + offY), dayText, (0, 0, 0), font=font)
     
+    
+    imgDraw.rectangle((offX, offY, sizeX + offX, s48 + offY), None, (100, 100, 100), width)
+
+    imgDraw.rectangle((offX, s48 + offY, sizeX + offX, s48 + sizey * 3 + offY), None, (100, 100, 100), width)
     imgDraw.line((offX, s48 + sizey + offY, sizeX + offX, s48 + sizey + offY), (100, 100, 100), width)
     imgDraw.line((offX, s48 + sizey * 2 + offY, sizeX + offX, s48 + sizey * 2 + offY), (100, 100, 100), width)
     imgDraw.line((s300 + offX, s48 + offY, s300 + offX, s48 + sizey * 3 + offY), (100, 100, 100), width)
     imgDraw.line((s500 + offX, s48 + offY, s500 + offX, s48 + sizey * 3 + offY), (100, 100, 100), width)
-    
-    
+
     output = BytesIO()
     img.save(output, format='PNG')
     return output
